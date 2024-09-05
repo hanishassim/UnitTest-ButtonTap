a39ee2c56f8feb33ac4f611502e9803bcd106ed3
Hanis <hanishassim@users.noreply.github.com>
2024-09-05 11:27:46 +0800

    Project init

---
+++ ButtonTap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ButtonTap
+//
+//  Created by Hassim, Muhammad Hanis on 05/09/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet private var button: UIButton!
+    
+    @IBAction private func buttonTap() {
+        print(">> Button was tapped")
+    }
+
+}
+
---
+++ ButtonTapTests/ViewControllerTests.swift
@@ -0,0 +1,8 @@
+@testable import ButtonTap
+import XCTest
+
+final class ViewControllerTests: XCTestCase {
+    func test_zero() throws {
+        XCTFail("Tests not yet implemented in ViewControllerTests")
+    }
+}
